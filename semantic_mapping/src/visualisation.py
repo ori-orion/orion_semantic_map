52c54e832b19b9086261e1f25e2f7092e4d12cb3
Matthew Munks <matthew.munks@exeter.ox.ac.uk>
2022-06-13 17:10:53 +0100

    Getting rid of the offending reference.

--- semantic_mapping/src/visualisation.py
+++ semantic_mapping/src/visualisation.py
@@ -98,65 +98,65 @@ class RvizVisualisationManager:
         return
 
 
-class Visualisation(object):
-    def __init__(self, coll_manager:CollectionManager.CollectionManager, server:InteractiveMarkerServer):
-        self.coll_manager:CollectionManager.CollectionManager = coll_manager
-        self.server:InteractiveMarkerServer = server
+# class Visualisation(object):
+#     def __init__(self, coll_manager:CollectionManager.CollectionManager, server:InteractiveMarkerServer):
+#         self.coll_manager:CollectionManager.CollectionManager = coll_manager
+#         self.server:InteractiveMarkerServer = server
 
-    def handle_viz_input(self, input):
-        if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
-            obj = self.coll_manager.queryIntoCollection(
-                {utils.PYMONGO_ID_SPECIFIER:pymongo.collection.ObjectId(input.marker_name)});
-            rospy.loginfo(obj);
-            rospy.loginfo("\n\n\n");
+#     def handle_viz_input(self, input):
+#         if (input.event_type == InteractiveMarkerFeedback.BUTTON_CLICK):
+#             obj = self.coll_manager.queryIntoCollection(
+#                 {utils.PYMONGO_ID_SPECIFIER:pymongo.collection.ObjectId(input.marker_name)});
+#             rospy.loginfo(obj);
+#             rospy.loginfo("\n\n\n");
 
-    def delete_object(self, id):
-        self.server.erase(id);
-        self.server.applyChanges();
+#     def delete_object(self, id):
+#         self.server.erase(id);
+#         self.server.applyChanges();
 
-    def update_objects(self, object:SOMObject, id):
+#     def update_objects(self, object:SOMObject, id):
 
-        # delete marker from object if its already there
-        self.server.erase(id)
+#         # delete marker from object if its already there
+#         self.server.erase(id)
 
-        int_marker = InteractiveMarker()
-        int_marker.header.frame_id = "map"
-        int_marker.name = id
-        int_marker.description = object.class_
-        int_marker.pose.position.x = object.obj_position.position.x;
-        int_marker.pose.position.y = object.obj_position.position.y;
-        int_marker.pose.position.z = object.obj_position.position.z;
-        int_marker.pose.orientation.w = 1
+#         int_marker = InteractiveMarker()
+#         int_marker.header.frame_id = "map"
+#         int_marker.name = id
+#         int_marker.description = object.class_
+#         int_marker.pose.position.x = object.obj_position.position.x;
+#         int_marker.pose.position.y = object.obj_position.position.y;
+#         int_marker.pose.position.z = object.obj_position.position.z;
+#         int_marker.pose.orientation.w = 1
 
-        box_marker = Marker()
-        box_marker.type = Marker.CUBE
-        box_marker.pose.orientation.w = 1
-        if object.size.x > 0.05:
-            box_marker.scale.x = object.size.x
-        else:
-            box_marker.scale.x = 0.05
-        if object.size.y > 0.05:
-            box_marker.scale.y = object.size.y
-        else:
-            box_marker.scale.y = 0.05
-        if object.size.z > 0.05:
-            box_marker.scale.z = object.size.z
-        else:
-            box_marker.scale.z = 0.05
-        box_marker.color.r = 0.0
-        box_marker.color.g = 0.2
-        box_marker.color.b = 1.0
-        box_marker.color.a = 0.7
+#         box_marker = Marker()
+#         box_marker.type = Marker.CUBE
+#         box_marker.pose.orientation.w = 1
+#         if object.size.x > 0.05:
+#             box_marker.scale.x = object.size.x
+#         else:
+#             box_marker.scale.x = 0.05
+#         if object.size.y > 0.05:
+#             box_marker.scale.y = object.size.y
+#         else:
+#             box_marker.scale.y = 0.05
+#         if object.size.z > 0.05:
+#             box_marker.scale.z = object.size.z
+#         else:
+#             box_marker.scale.z = 0.05
+#         box_marker.color.r = 0.0
+#         box_marker.color.g = 0.2
+#         box_marker.color.b = 1.0
+#         box_marker.color.a = 0.7
 
-        button_control = InteractiveMarkerControl()
-        button_control.interaction_mode = InteractiveMarkerControl.BUTTON
-        button_control.always_visible = True
-        button_control.markers.append(box_marker);
-        int_marker.controls.append(button_control);
+#         button_control = InteractiveMarkerControl()
+#         button_control.interaction_mode = InteractiveMarkerControl.BUTTON
+#         button_control.always_visible = True
+#         button_control.markers.append(box_marker);
+#         int_marker.controls.append(button_control);
 
-        self.server.insert(int_marker, self.handle_viz_input)
-        self.server.applyChanges();
-        return
+#         self.server.insert(int_marker, self.handle_viz_input)
+#         self.server.applyChanges();
+#         return
 
     # def rois_to_marker_array(self, rois):
     #     markers = []
