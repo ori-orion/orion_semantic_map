4dd68fa8dbbcc2f554600f03850979de089eb3d3
Matthew Munks <matthewmunks@gmail.com>
2022-11-14 16:12:50 +0000

    Suppressing the double detection of humans.

--- semantic_mapping/src/semantic_mapping/main.py
+++ semantic_mapping/src/semantic_mapping/main.py
@@ -227,8 +227,8 @@ class MemSys:
             last_observation_batch="last_observation_batch",
             positional_covariance_attr="covariance_mat",
             observation_counter_attr="num_observations",
-            suppress_double_detections=False,       # Currently the suppression of double detections if off!
-            suppression_distance_dict={'suppression_test_type':0.1}
+            suppress_double_detections=True,       # Currently the suppression of double detections if off!
+            suppression_distance_dict={'suppression_test_type':0.1, 'person':0.5}
         );
         self.observation_arg_name_defs.dont_transfer.append("covariance_mat");
         self.observation_arg_name_defs.dont_transfer.append("transform_cov_to_diagonal");
