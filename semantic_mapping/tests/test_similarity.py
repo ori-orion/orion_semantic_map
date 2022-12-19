e085db50ce5fe9a81c06235bdb389f4186c20c74
Matteo Cati <oettam.itac@gmail.com>
2022-12-19 17:10:15 +0100

    Implement test_similarity.py with rostest

--- semantic_mapping/tests/test_similarity.py
+++ semantic_mapping/tests/test_similarity.py
@@ -1,53 +1,44 @@
 #!/usr/bin/env python3
 
 import rospy
-
 from orion_actions.srv import SOMSimilarityQuery, SOMSimilarityQueryRequest, SOMSimilarityQueryResponse
 
-def test_similarity(similarity_service: rospy.ServiceProxy):
-    """Test similarity for two different terms is between 0 and 1"""
-    print('Testing similarity between two different terms')
-    
-    query = SOMSimilarityQueryRequest('Sour_Candy_bag', 'Gummy_Candy_bag')
-    response: SOMSimilarityQueryResponse = similarity_service(query)
+import unittest
 
-    assert 0 < response.similarity < 1, f'Similarity should be between 0 and 1, received {response.similarity}'
+class TestOntologySimilarity(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        cls.similarity_service = rospy.ServiceProxy('/som/similarity/get_similarity', SOMSimilarityQuery)
 
-    print('\tDone')
+    def test_similarity_is_between_zero_and_one(self):
+        """Test that similarity for two different terms is between 0 and 1."""
+        
+        query = SOMSimilarityQueryRequest('Sour_Candy_bag', 'Gummy_Candy_bag')
+        response: SOMSimilarityQueryResponse = self.similarity_service(query)
 
-def test_similarity_same_object(similarity_service: rospy.ServiceProxy):
-    """Test that similarity of two terms that are equal is 1"""
-    print('Testing similarity when terms are equal')
-    
-    query = SOMSimilarityQueryRequest('Sour_Candy_bag', 'Sour_Candy_bag')
-    response: SOMSimilarityQueryResponse = similarity_service(query)
+        self.assertGreater(response.similarity, 0, 'Similarity should be greater than 0 for two different terms.')
+        self.assertLess(response.similarity, 1, 'Similarity should be less than 1 for two different terms.')
 
-    assert response.similarity == 1, f'Similarity should be 1 when two terms are equal, received {response.similarity}'
-    
-    print('\tDone')
+    def test_similarity_same_terms_is_one(self):
+        """Test that similarity of two terms that are equal is 1."""
+        
+        query = SOMSimilarityQueryRequest('Sour_Candy_bag', 'Sour_Candy_bag')
+        response: SOMSimilarityQueryResponse = self.similarity_service(query)
 
-def test_similarity_object_not_present(similarity_service: rospy.ServiceProxy):
-    """Test that similarity is 0 if an object is not present"""
-    print('Testing similarity when a term is not present')
+        self.assertEqual(response.similarity, 1, 'Similarity should be 1 when two terms are equal.')
+        
+    def test_similarity_with_unknown_term_is_zero(self):
+        """Test that similarity is 0 if one of the terms is not present in the ontology."""
 
-    query1 = SOMSimilarityQueryRequest('object_not_present', 'Sour_Candy_bag')
-    query2 = SOMSimilarityQueryRequest('Sour_Candy_bag', 'object_not_present')
+        query1 = SOMSimilarityQueryRequest('object_not_present', 'Sour_Candy_bag')
+        query2 = SOMSimilarityQueryRequest('Sour_Candy_bag', 'object_not_present')
 
-    response1: SOMSimilarityQueryResponse = similarity_service(query1)
-    response2: SOMSimilarityQueryResponse = similarity_service(query2)
+        response1: SOMSimilarityQueryResponse = self.similarity_service(query1)
+        response2: SOMSimilarityQueryResponse = self.similarity_service(query2)
 
-    assert response1.similarity == 0, f'Similarity should be 0 when first term not present in the ontology, received {response1.similarity}'
-    assert response2.similarity == 0, f'Similarity should be 0 when second term not present in the ontology, received {response2.similarity}'
-
-    print('\tDone')
-
+        self.assertEqual(response1.similarity, 0, 'Similarity should be 0 when first term not present in the ontology')
+        self.assertEqual(response2.similarity, 0, 'Similarity should be 0 when second term not present in the ontology')
 
 if __name__ == '__main__':
-    rospy.init_node('som_test_similarity_node')
-
-    similarity_service = rospy.ServiceProxy('/som/similarity/get_similarity', SOMSimilarityQuery)
-
-    test_similarity(similarity_service)
-    test_similarity_same_object(similarity_service)
-    test_similarity_object_not_present(similarity_service)
-
+    import rostest
+    rostest.rosrun('semantic_mapping', 'test_ontology_similarity', 'test_similarity.TestOntologySimilarity')
